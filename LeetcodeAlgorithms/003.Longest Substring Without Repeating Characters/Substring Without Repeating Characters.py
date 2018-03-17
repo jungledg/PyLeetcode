d14269c884e67c6451ca08bfabf8d88a6b851843
高正东 <gaozhengdong1994@gmail.com>
2018-03-17 20:34:12 +0800

    finish "Longest Substring Without Repeating Characters"

--- LeetcodeAlgorithms/003.Longest Substring Without Repeating Characters/Substring Without Repeating Characters.py
+++ LeetcodeAlgorithms/003.Longest Substring Without Repeating Characters/Substring Without Repeating Characters.py
@@ -13,4 +13,22 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        d = {}
+        record_lenth = 0
+        for num, ch in enumerate(s):
+            if ch not in d:
+                d[ch] = num
+            else:
+                old_mark = d[ch]
+                for key in list(d.keys()):
+                    if d[key] <= old_mark:
+                        del(d[key])
+                d[ch] = num
+            long = len(d)
+            if long > record_lenth:
+                record_lenth = long
+        return record_lenth
 
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.lengthOfLongestSubstring("pwwkewww"))
