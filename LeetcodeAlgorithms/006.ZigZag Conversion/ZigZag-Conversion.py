4c6f84853c9c33a5b1885f247102c4c94af16492
高正东 <gaozhengdong1994@gmail.com>
2018-03-20 23:55:33 +0800

    begin "ZigZag-Conversion"

---
+++ LeetcodeAlgorithms/006.ZigZag Conversion/ZigZag-Conversion.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Author:Dalon Gao
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/20 23:54
+# @Author  : Dallo Gao
+# @Site    : 
+# @File    : ZigZag-Conversion.py
+# @Software: PyCharm
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
