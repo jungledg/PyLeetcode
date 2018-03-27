710dd4c6b58c50b98bc8ae6784b01133fb15b17f
高正东 <gaozhengdong1994@gmail.com>
2018-03-27 23:32:48 +0800

    fi "ZigZag"

--- LeetcodeAlgorithms/006.ZigZag Conversion/ZigZag-Conversion.py
+++ LeetcodeAlgorithms/006.ZigZag Conversion/ZigZag-Conversion.py
@@ -13,3 +13,24 @@ class Solution:
         :type numRows: int
         :rtype: str
         """
+        if numRows < 2 or len(s) <= numRows:
+            return s
+        re = ''
+        for i in range(numRows):
+            count = i
+            while count < len(s):
+                re += s[count]
+                if i == 0 or i == (numRows - 1):
+                    count += 2*numRows - 2
+                else:
+                    count += 2 * (numRows - i) - 2
+                    if count >= len(s):
+                        break
+                    re += s[count]
+                    count += 2*i
+
+        return re
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.convert("ABCDE", 4))
